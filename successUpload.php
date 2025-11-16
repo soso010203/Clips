8343de3a8cdb92387b24b1aa8f81ab9729b60a24
soso010203 <if25b259@technikum-wien.at>
2025-11-16 22:33:09 +0100

    css für upload

--- successUpload.php
+++ successUpload.php
@@ -9,6 +9,7 @@ $successfulUpload= $_SESSION['successfulUpload'] ?? "Upload successful!";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>Upload-Successful</title>
  </head>
 <body>
@@ -17,7 +18,6 @@ $successfulUpload= $_SESSION['successfulUpload'] ?? "Upload successful!";
 
 <div class="Uploadsuccessful">
     <h1>Upload successful!</h1>
-    <p><?php echo $successfulUpload; ?></p>
     <p>You can view your post under your <a href="profile.php">Profile</a> now.</p>
 </div>
 
