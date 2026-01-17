d793ca6c0c0b62ed9178cb7f141c8ddad189bdc4
soso010203 <if25b259@technikum-wien.at>
2026-01-17 23:08:14 +0100

    Homepage angepasst

--- index.php
+++ index.php
@@ -1,73 +1,97 @@
+<?php
+session_start();
+
+require_once 'config/db.php'; //connects to the database
+
+$currentUserId = $_SESSION['user']['id'] ?? null; 
+
+if ($currentUserId) {
+    // If a user is logged in, their own posts shouldn't be displayed
+    $stmt = $pdo->prepare("
+        SELECT *
+        FROM posts
+        WHERE user_id != ?
+        ORDER BY created_at DESC
+        LIMIT 6
+    ");
+    $stmt->execute([$currentUserId]);
+} else {
+    // Not logged in → just latest 6 posts
+    $stmt = $pdo->query("
+        SELECT *
+        FROM posts
+        ORDER BY created_at DESC
+        LIMIT 6
+    ");
+}
+
+$posts = $stmt->fetchAll();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="stylesheet.css">
-
-    <title>Document</title>
-
+    <title>Home</title>
 </head>
-
-<header>
-
-<!-- Navbar -->
-<?php include 'parts/navbar.php';?> 
-
-
-</header>
-
 <body>
 
-<div class="container d-flex flex-column align-items-center"> 
+<?php include 'parts/navbar.php'; ?>
 
+<div class="container d-flex flex-column align-items-center">
 
-<h1 class="text-center m-5"> hello @ clips!</h1>
+    <h1 class="text-center m-5">
+        <?php if ($currentUserId): ?>
+            Hello
+        <?php else: ?>
+            Welcome!
+        <?php endif; ?>
+    </h1>
 
-<!-- Row: bei md+ zwei Spalten (col-md-6), sonst eine (col-12).
-     Auf kleinen Bildschirmen centered, auf md+ linksbündig innerhalb
-     eines zentrierten Bereichs (so bleibt der einzelne letzte Post links). -->
-<div class="row justify-content-md-start " style="max-width:90%;">
+    <div class="row justify-content-md-start" style="max-width:90%;">
 
-  <div class="col-12 col-md-6 d-flex">
-    <div class="card w-100">
-      <img src="pics/Tier01.jpg" class="card-img-top" alt="Tier01">
-      <div class="card-body">
-        <p class="card-text">Today i saw an interesting bird. Look at the photo!</p>
-        <a href="post.php?id=1" class="stretched-link"></a>
-      </div>
+        <?php if (count($posts) === 0): ?>
+            <p class="text-center">Noch keine Posts vorhanden.</p>
+        <?php endif; ?>
+
+        <?php foreach ($posts as $post): ?>
+            <div class="col-12 col-md-6 d-flex mb-4">
+                <div class="card w-100">
+
+                    <!-- Post Image -->
+                    <img
+                        src="<?php echo htmlspecialchars($post['file_path']); ?>"
+                        class="card-img-top"
+                        alt="Post image"
+                    >
+
+                    <!-- Caption -->
+                    <div class="card-body">
+                        <p class="card-text" style="
+                            display: -webkit-box;
+                            -webkit-line-clamp: 2;
+                            -webkit-box-orient: vertical;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                        ">
+                            <?php echo htmlspecialchars($post['text']); ?>
+                        </p>
+
+                        <!-- Link to Post Detail -->
+                        <a href="post.php?id=<?php echo $post['id']; ?>" class="stretched-link"></a>
+                    </div>
+
+                </div>
+            </div>
+        <?php endforeach; ?>
+
     </div>
-  </div>
-
-  <div class="col-12 col-md-6 d-flex">
-    <div class="card w-100">
-      <img src="pics/Landschaft.png" class="card-img-top" alt="Landschaft">
-      <div class="card-body">
-        <p class="card-text">Look at the beautiful landscape!</p>
-        <a href="post.php?id=2" class="stretched-link"></a>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-12 col-md-6 d-flex">
-    <div class="card w-100">
-      <img src="pics/Winter.jpg" class="card-img-top" alt="Winter">
-      <div class="card-body">
-        <p class="card-text">What a beautiful winter day.</p>
-        <a href="post.php?id=3" class="stretched-link"></a>
-      </div>
-    </div>
-  </div>
-
 </div>
 
-</div> 
-
-<!-- Navbar -->
-<?php include 'parts/footer.php';?> 
+<?php include 'parts/footer.php'; ?>
 
 </body>
 </html>
